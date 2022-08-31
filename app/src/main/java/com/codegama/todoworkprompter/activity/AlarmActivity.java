20c31415168a69caa5194e7a66809c7f15f4fbff
pradeep0143 <kamatepradeep730@gmail.com>
2022-08-31 10:23:25 +0530

    first commit

---
+++ app/src/main/java/com/codegama/todoworkprompter/activity/AlarmActivity.java
@@ -0,0 +1,89 @@
+package com.codegama.todolistapplication.activity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.codegama.todolistapplication.R;
+
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AlarmActivity extends BaseActivity {
+
+    private static AlarmActivity inst;
+    @BindView(R.id.imageView)
+    ImageView imageView;
+    @BindView(R.id.title)
+    TextView title;
+    @BindView(R.id.description)
+    TextView description;
+    @BindView(R.id.timeAndData)
+    TextView timeAndData;
+    @BindView(R.id.closeButton)
+    Button closeButton;
+    MediaPlayer mediaPlayer;
+    TextToSpeech t1;
+
+    public static AlarmActivity instance() {
+        return inst;
+    }
+
+    public void ConvertText(View view)
+    {
+        String s=title.getText().toString();
+        t1.speak(s,TextToSpeech.QUEUE_FLUSH,null,null);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+        ButterKnife.bind(this);
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener()
+        {
+            @Override
+            public void onInit(int i)
+            {
+                if (i!=TextToSpeech.ERROR)
+                {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        String s=title.getText().toString();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.notification);
+        mediaPlayer.getSelectedTrack(t1.speak(s, TextToSpeech.QUEUE_FLUSH, null, null));
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+
+        if(getIntent().getExtras() != null) {
+            title.setText(getIntent().getStringExtra("TITLE"));
+            description.setText(getIntent().getStringExtra("DESC"));
+            timeAndData.setText(getIntent().getStringExtra("DATE") + ", " + getIntent().getStringExtra("TIME"));
+        }
+
+        Glide.with(getApplicationContext()).load(R.drawable.alert).into(imageView);
+        closeButton.setOnClickListener(view -> finish());
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();
+    }
+}
